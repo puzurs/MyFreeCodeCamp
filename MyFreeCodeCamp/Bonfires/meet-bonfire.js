186b24bc8c72b25ef93d58324e9f9b0b39f11e6b
puzurs <maris@Mariss-MacBook-Pro.local>
2015-12-14 09:49:28 +0200

    Bonfire

---
+++ MyFreeCodeCamp/Bonfires/meet-bonfire.js
@@ -0,0 +1,17 @@
+/// Bonfire: Meet Bonfire
+/* Your goal is to fix the failing test.
+
+First, run all the tests by clicking "Run tests" or by pressing Control + Enter.
+
+The failing test is in red. Fix the code so that all tests pass. Then you can move on to the next Bonfire.
+
+Make this function return true no matter what. */
+
+function meetBonfire(argument) {
+  // Good luck!
+  console.log("you can read this function's argument in the developer tools", argument);
+
+  return true;
+}
+
+meetBonfire("You can do this!");
